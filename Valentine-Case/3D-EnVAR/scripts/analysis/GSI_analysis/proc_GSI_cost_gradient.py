a7a6555272d6774a82d819c3308127c1e37eccae
cgrudz <cgrudzien@comet-ln3.sdsc.edu>
2022-09-15 14:40:16 -0700

    Untested bugfix on download_GEFS, finished initial fort.220 processing script

--- Valentine-Case/3D-EnVAR/scripts/analysis/GSI_analysis/proc_GSI_cost_gradient.py
+++ Valentine-Case/3D-EnVAR/scripts/analysis/GSI_analysis/proc_GSI_cost_gradient.py
@@ -30,7 +30,9 @@ import numpy as np
 import pandas as pd
 import pickle
 import copy
-import datetime
+from datetime import datetime as dt
+from datetime import timedelta
+import ipdb
 
 ##################################################################################
 # SET GLOBAL PARAMETERS 
@@ -41,10 +43,10 @@ DATA_ROOT = PROJ_DIR + '/data/cycle_io'
 OUT_DIR = PROJ_DIR + '/data/analysis'
 
 # starting date and zero hour of data
-START_DATE = '2019-02-11T00:00:00'
+START_DATE = '2019-02-08T00:00:00'
 
 # final date and zero hour of data
-END_DATE = '2019-02-11T00:00:00'
+END_DATE = '2019-02-15T06:00:00'
 
 # number of hours between zero hours for forecast data
 CYCLE_INT = 6
@@ -60,36 +62,47 @@ str_indt = '    '
 def get_anls(start_date, end_date, cycle_int):
     # generates analysis times based on script parameters
     anl_dates = []
+    anl_strng = []
     delta = end_date - start_date
-    hours_range = delta.total_seconds() / 360
-    fcst_steps = int(max_fcst / fcst_int)
+    hours_range = delta.total_seconds() / 3600
 
     if cycle_int == 0 or delta.total_seconds() == 0:
         # for a zero cycle interval or start date equal end date, only process 
-        # start date / hour directory
-        anl_dates.append([start_date.strftime('%Y%m%d%H'))
+        # the start date time directory
+        anl_dates.append(start_date)
+        anl_strng.append(start_date.strftime('%Y%m%d%H'))
 
     else:
-        # define the analysis time over range of cycle intervals
+        # define the analysis times over range of cycle intervals
         cycle_steps = int(hours_range / cycle_int)
+        ipdb.set_trace()
         for i in range(cycle_steps + 1):
-            anl_start = start_date + timedelta(hours=(i * cycle_int))
-            anl_dates.append(anl_start.strftime('%Y%m%d%H'))
+            anl_date = start_date + timedelta(hours=(i * cycle_int))
+            anl_dates.append(anl_date)
+            anl_strng.append(anl_date.strftime('%Y%m%d%H'))
 
-    return anl_dates
+    return zip(anl_dates, anl_strng)
 
 
 ##################################################################################
 # Process data
 
+# convert to date times
+start_date = dt.fromisoformat(START_DATE)
+end_date = dt.fromisoformat(END_DATE)
+
+# define the output name
+out_path = OUT_DIR + '/GSI_cost_grad_anl_' + START_DATE +\
+           '_to_' + END_DATE + '.txt'
+
 # generate the date range for the analyses
-anl_dates = get_anls(start_date, end_date, cycle_int):
+analyses = get_anls(start_date, end_date, CYCLE_INT)
 
 # initiate empty dataframe / dictionary
 d0 = pd.DataFrame.from_dict({
     'date' : [],
+    'loop' : [],
     'iter' : [],
-    'step' : [],
     'cost' : [],
     'grad' : [],
     })
@@ -98,34 +111,38 @@ for i in range(1, MAX_DOM + 1):
     # define a new dictionary for each domain
     exec('d0%s = copy.copy(d0)'%i)
 
+    print('Processing domain d0%s'%i)
+
     # define the line index for the dataframe
-    step = 1
+    step = 0
     
-    for anl in anl_dates:
+    for (anl_date, anl_strng) in analyses:
         # open file and loop lines
-        in_path = DATA_ROOT + '/' + anl + '/gsiprd/d0' + str(i) + '/fort.220'
+        in_path = DATA_ROOT + '/' + anl_strng + '/gsiprd/d0' + str(i) + '/fort.220'
+        print(str_indt + 'Opening file ' + in_path)
         f = open(in_path)
 
         for line in f:
             split_line = line.split(',')
             prefix = split_line[0]
             if prefix == 'cost':
-               split_line = split_line[-1].split() 
-                # process domain updates within MAX_DOM
-                    if split_line[4:6] == ['dpsdt,', 'dmudt']:
-                        tmp = np.array(split_line[7:])
-                        tmp_dict = {
-                            'wrf_time' : [date_time],
-                            'xtime'    : [float(tmp[0])],      
-                            'dpsdt'    : [float(tmp[1])],
-                            'dmudt'    : [float(tmp[2])], 
-                            }
-                        exec('tmp_dict[\'step\'] = [int(d0%s_indx)]'%i)
-                        tmp_dict = pd.DataFrame.from_dict(tmp_dict, orient='columns')
-                        exec('d0%s = pd.concat([d0%s, tmp_dict], axis=0)'%(i,i))
-                        exec('d0%s_indx += 1'%i)
+                step += 1
+                split_line = split_line[-1].split() 
+                tmp = np.array(split_line[2:6])
+                tmp_dict = {
+                    'date' : [anl_date],
+                    'loop' : [float(tmp[0])],      
+                    'iter' : [float(tmp[1])],
+                    'cost' : [float(tmp[2])], 
+                    'grad' : [float(tmp[3])], 
+                    }
+                exec('tmp_dict[\'step\'] = [int(step)]')
+                tmp_dict = pd.DataFrame.from_dict(tmp_dict, orient='columns')
+                exec('d0%s = pd.concat([d0%s, tmp_dict], axis=0)'%(i,i))
     
-f.close()
+        print(str_indt + 'Closing file ' + in_path)
+        f.close()
+
 data = {}
 
 for i in range(1, MAX_DOM + 1):
@@ -133,7 +150,8 @@ for i in range(1, MAX_DOM + 1):
     exec('d0%s = d0%s.set_index(\'step\')'%(i,i))
     exec('data[\'d0%s\'] = d0%s'%(i,i))
 
-f = open(OUT_PATH, 'wb')
+print('Writing out data to ' + out_path)
+f = open(out_path, 'wb')
 pickle.dump(data, f)
 f.close()
 
