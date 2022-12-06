7e85cb11a8c77e4fee39ebc4e20ca3f4f12fbb77
cgrudz <cgrudzien@comet-29-04.sdsc.edu>
2022-12-06 14:13:33 -0800

    Staging test of Michael's 3D-VAR experiment as sub-case of 3D-EnVAR

--- Common-Case/3D-EnVAR/rocoto_utilities.py
+++ Common-Case/3D-EnVAR/rocoto_utilities.py
@@ -35,15 +35,16 @@ import os
 # SET GLOBAL PARAMETERS
 ##################################################################################
 # directory for git clone of GSI-WRF-Cycling-Template
-USR_HME = '/cw3e/mead/projects/cwp130/scratch/'
+USR_HME = '/cw3e/mead/projects/cwp106/scratch/cgrudzien'
 
 # directory for rocoto install
 RCT_HME = '/cw3e/mead/projects/cwp130/scratch/cgrudzien'
 
 # name of .xml workflow WITHOUT the extension
+CTR_FLW = '3dvar'
 #CTR_FLW = '3denvar_wps_perts'
 #CTR_FLW = '3denvar_enkf'
-CTR_FLW = '3denvar_downscale'
+#CTR_FLW = '3denvar_downscale'
 #CTR_FLW = 'ensemble_forecast'
 #CTR_FLW = 'deterministic_forecast'
 
@@ -66,7 +67,22 @@ dbs_dir = PRJ_DIR + '/data'
 # Rocoto utility commands
 ##################################################################################
 # The following commands are wrappers for the native rocoto functions described
-# in the documentation http://christopherwharrop.github.io/rocoto/
+# in the documentation:
+#
+#     http://christopherwharrop.github.io/rocoto/
+#
+# The rocoto run and stat commands require no arguments and are defined by the
+# global parameters in the above sections. For the boot and rewind commands, one
+# should supply a list of strings corresponding to the cycle / task names.  One
+# can, e.g., loop through ensemble indexed tasks this way with an iterator of
+# the form:
+#
+#    run_rocotoboot(['201902090000'],
+#                   ['ungrib_ens_' + str(i).zfill(2) for i in range(21)])
+#
+# to boot all tasks in a range.
+#
+##################################################################################
 
 def run_rocotorun():
     cmd = PATHROC + '/bin/rocotorun -w ' +\
@@ -82,21 +98,27 @@ def run_rocotostat():
 
     os.system(cmd) 
 
-def run_rocotoboot(cycle, task_list):
-    cmd = PATHROC + '/bin/rocotoboot -w ' +\
-          flw_dir + '/' + CTR_FLW + '.xml' +\
-          ' -d ' + dbs_dir + '/workflow/' + CTR_FLW + '.store' +\
-          ' -c ' + cycle + ' -t ' + task_list
+def run_rocotoboot(cycles, tasks):
+    # this will loop over the list of cycles and tasks and boot them
+    for cycle in cycles:
+        for task in tasks:
+            cmd = PATHROC + '/bin/rocotoboot -w ' +\
+                  flw_dir + '/' + CTR_FLW + '.xml' +\
+                  ' -d ' + dbs_dir + '/workflow/' + CTR_FLW + '.store' +\
+                  ' -c ' + cycle + ' -t ' + task
 
-    os.system(cmd) 
+            os.system(cmd) 
 
-def run_rocotorewind(cycle, task_list):
-    cmd = PATHROC + '/bin/rocotorewind -w ' +\
-          flw_dir + '/' + CTR_FLW + '.xml' +\
-          ' -d ' + dbs_dir + '/workflow/' + CTR_FLW + '.store' +\
-          ' -c ' + cycle + ' -t ' + task_list
+def run_rocotorewind(cycles, tasks):
+    # this will loop over the list of cycles and tasks and rewind them
+    for cycle in cycles:
+        for task in tasks:
+            cmd = PATHROC + '/bin/rocotorewind -w ' +\
+                  flw_dir + '/' + CTR_FLW + '.xml' +\
+                  ' -d ' + dbs_dir + '/workflow/' + CTR_FLW + '.store' +\
+                  ' -c ' + cycle + ' -t ' + task
 
-    os.system(cmd) 
+            os.system(cmd) 
 
 ##################################################################################
 # end
