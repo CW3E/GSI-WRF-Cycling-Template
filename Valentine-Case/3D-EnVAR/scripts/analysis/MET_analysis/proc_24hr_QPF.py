972b08e0c1ada7a884a617f24c4912f242734a7e
cgrudz <cgrudzien@comet-ln2.sdsc.edu>
2022-12-09 14:46:15 -0800

    Successful test of batch processing MET output, moving to plotting diagnostics

--- Valentine-Case/3D-EnVAR/scripts/analysis/MET_analysis/proc_24hr_QPF.py
+++ Valentine-Case/3D-EnVAR/scripts/analysis/MET_analysis/proc_24hr_QPF.py
@@ -1,27 +1,10 @@
 ##################################################################################
 # Description
 ##################################################################################
-# This script reads in a generic rsl.*.* file from a WRF run and creates a Pandas
-# dataframe containing time series values of the outputs of lines
-#
-#     d0X   Domain average of dpsdt, dmudt (mb/3h): xtime dpsdt dmudt 
-#     d0X   Max mu change time step: xgrid ygrid maxdmu 
-#
-# The dataframes are saved into a Pickled dictionary organized by domain number
-# 'd0X', with the dataframe columns given as 
-#
-#    'step'      : integer time step of the simulation - domain specific
-#    'wrf_time'  : the raw model hour of the simulation in datetime format
-#    'xtime'     : total ellapsed minutes of model time in decimal form
-#    'dpsdt'     : surface pressure tendency
-#    'dmudt'     : mu tendency
-#    'xgrid'     : max change in mu over grid -- corresponing x coordinate
-#    'ygrid'     : max change in mu over grid -- corresponing y coordinate
-#    'maxdmu'    : max change in mu over grid
-#
-# Data input and output directories should be defined in the below along with
-# MAX_DOM to control the number of domains processed.  Testing on more than two
-# domains is still pending.
+# This script reads in grid_stat_* files from a MET analysis and creates a Pandas
+# dataframe containing a time series versus lead time to a verification period.
+# The dataframes are saved into a Pickled dictionary organized by MET file
+# extension
 #
 ##################################################################################
 # License Statement
@@ -52,128 +35,107 @@ import glob
 from datetime import datetime as dt
 from datetime import timedelta
 from py_plt_utilities import STR_INDT, get_anls, PROJ_ROOT
+import ipdb
 
 ##################################################################################
 # SET GLOBAL PARAMETERS 
 ##################################################################################
 # define control flow to analyze 
-CTR_FLW = '3denvar_downscale'
+CTR_FLW = 'deterministic_forecast_early_start_date_test'
 
-# starting date and zero hour of data
-START_DATE = '2019-02-07T18:00:00'
+# starting date and zero hour of forecast cycles
+START_DATE = '2019-02-11T00:00:00'
 
-# final date and zero hour of data
-END_DATE = '2019-02-08T06:00:00'
+# final date and zero hour of data of forecast cycles
+END_DATE = '2019-02-14T00:00:00'
+
+# valid date for the verification
+VALID_DATE = '2019-02-15T00:00:00'
 
 # number of hours between zero hours for forecast data
-CYCLE_INT = 6
-
-# define domains to process
-MAX_DOM = 1
+CYCLE_INT = 24
 
 ##################################################################################
 # Process data
 ##################################################################################
 # define derived data paths 
-data_root = PROJ_ROOT + '/data/cycle_io/' + CTR_FLW
-out_dir = PROJ_ROOT + '/data/analysis/' + CTR_FLW
+data_root = PROJ_ROOT + '/data/analysis/' + CTR_FLW + '/MET_analysis'
+out_dir = PROJ_ROOT + '/data/analysis/' + CTR_FLW + '/MET_analysis'
 
 # convert to date times
 start_date = dt.fromisoformat(START_DATE)
 end_date = dt.fromisoformat(END_DATE)
+valid_date = dt.fromisoformat(VALID_DATE)
 
 # define the output name
-out_path = out_dir + '/WRF_dps_dmu_dt_' + START_DATE +\
-           '_to_' + END_DATE + '.bin'
+out_path = out_dir + '/grid_stats_lead_' + START_DATE +\
+           '_to_' + END_DATE + '_valid_' + VALID_DATE +\
+           '.bin'
 
 # generate the date range for the analyses
 analyses = get_anls(start_date, end_date, CYCLE_INT)
 
-# initiate empty dataframe / dictionary
-d0 = pd.DataFrame.from_dict({
-    'step'     : [],
-    'wrf_time' : [],
-    'xtime'    : [],
-    'dpsdt'    : [],
-    'dmudt'    : [],
-    'xgrid'    : [],
-    'ygrid'    : [],
-    'maxdmu'   : [],
-    })
-
-for i in range(1, MAX_DOM + 1):
-    # define a new dictionary for each domain
-    exec('d0%s = copy.copy(d0)'%i)
-
-    # define the step index
-    exec('d0%s_indx = 1'%i)
+# initiate empty dictionary for storage of dataframes by keyname
+data_dict = {}
 
 print('Processing dates ' + START_DATE + ' to ' + END_DATE)
 for (anl_date, anl_strng) in analyses:
-    # define the rsl.error.0000 file to open based on the analysis date
-    in_path = data_root + '/' + anl_strng + '/wrfprd/ens_00/rsl.wrf.*'
+    # define the gridstat files to open based on the analysis date
+    in_paths = data_root + '/' + anl_strng + '/grid_stat_*.txt'
 
-    # find the lexicographically last rsl directory based on run times
-    in_path = sorted(glob.glob(in_path))[-1]
-    in_path = in_path + '/rsl.error.0000'
-    print(STR_INDT + 'Opening file ' + in_path)
+    # loop sorted grid_stat_* files
+    in_paths = sorted(glob.glob(in_paths))
+    for in_path in in_paths:
+        print(STR_INDT + 'Opening file ' + in_path)
 
-    # open file and loop lines
-    f = open(in_path)
-    for line in f:
-        split_line = line.split()
-        prefix = split_line[0]
-        if prefix == 'Timing':
-            # update wrf_time handling exceptions
-            try:
-                t = split_line[6]
-                date, time = t.split('_')
-                date_time = pd.to_datetime(date + ' ' + time)
-                print(2 * STR_INDT + str(date_time))
-            except:
-                pass
-    
-        elif prefix[:2] == 'd0':
-            # process domain updates within MAX_DOM
-            i = prefix[-1]
-            if int(i) <= MAX_DOM:
-                if split_line[4:6] == ['dpsdt,', 'dmudt']:
-                    try:
-                        tmp = np.array(split_line[7:])
-                        tmp_dict = {
-                            'wrf_time' : [date_time],
-                            'xtime'    : [float(tmp[0])],      
-                            'dpsdt'    : [float(tmp[1])],
-                            'dmudt'    : [float(tmp[2])], 
-                            }
-                    except:
-                        pass
-    
-                elif split_line[1:3] == ['Max', 'mu']:
-                    try:
-                        tmp = np.array(split_line[6:])
-                        tmp_dict['xgrid'] = [float(tmp[0])]  
-                        tmp_dict['ygrid'] = [float(tmp[1])] 
-                        tmp_dict['maxdmu'] = [float(tmp[2])] 
-                        exec('tmp_dict[\'step\'] = [int(d0%s_indx)]'%i)
-                        tmp_dict = pd.DataFrame.from_dict(tmp_dict, orient='columns')
-                        exec('d0%s = pd.concat([d0%s, tmp_dict], axis=0)'%(i,i))
-                        exec('d0%s_indx += 1'%i)
-                    except:
-                        pass
-    
-    print(STR_INDT + 'Closing file ' + in_path)
-    f.close()
-data = {}
+        # cut the diagnostic type from file name
+        fname = in_path.split('/')[-1]
+        split_name = fname.split('_')
+        postfix = split_name[-1].split('.')
+        postfix = postfix[0]
 
-for i in range(1, MAX_DOM + 1):
-    exec('d0%s[\'step\'] = d0%s[\'step\'].astype(int)'%(i,i))
-    exec('d0%s = d0%s.set_index(\'step\')'%(i,i))
-    exec('data[\'d0%s\'] = d0%s'%(i,i))
+        # open file, load column names, then loop lines
+        f = open(in_path)
+        cols = f.readline()
+        cols = cols.split()
+        
+        fname_df = {} 
+        tmp_dict = {}
+        df_indx = 1
+
+        print(STR_INDT + 'Loading columns:')
+        for col_name in cols:
+            print(STR_INDT * 2 + col_name)
+            fname_df[col_name] = [] 
+
+        fname_df =  pd.DataFrame.from_dict(fname_df, orient='columns')
+
+        # parse file by line, concatenating columns
+        for line in f:
+            split_line = line.split()
+
+            for i in range(len(split_line)):
+                tmp_dict[cols[i]] = split_line[i]
+
+            tmp_dict['step'] = [int(df_indx)]
+            tmp_dict = pd.DataFrame.from_dict(tmp_dict, orient='columns')
+            fname_df = pd.concat([fname_df, tmp_dict], axis=0)
+            df_indx += 1
+        
+        fname_df = fname_df.set_index('step')
+
+        if postfix in data_dict.keys():
+            data_dict[postfix] = pd.concat([data_dict[postfix], fname_df], axis=0)
+
+        else:
+            data_dict[postfix] = fname_df
+
+        print(STR_INDT + 'Closing file ' + in_path)
+        f.close()
 
 print('Writing out data to ' + out_path)
 f = open(out_path, 'wb')
-pickle.dump(data, f)
+pickle.dump(data_dict, f)
 f.close()
 
 ##################################################################################
