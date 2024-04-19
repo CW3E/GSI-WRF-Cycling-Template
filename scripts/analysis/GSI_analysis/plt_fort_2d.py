036650dbfbe1ee87557f1016b213fa8131f9208e
Colin James Grudzien <cgrudzien@login01.expanse.sdsc.edu>
2024-04-18 17:49:46 -0700

    Replacing license with UCSD standard

--- scripts/analysis/GSI_analysis/plt_fort_2d.py
+++ scripts/analysis/GSI_analysis/plt_fort_2d.py
@@ -25,22 +25,41 @@
 # than one domain is still pending.
 #
 ##################################################################################
-# License Statement
+# License Statement:
 ##################################################################################
+# This software is Copyright © 2024 The Regents of the University of California.
+# All Rights Reserved. Permission to copy, modify, and distribute this software
+# and its documentation for educational, research and non-profit purposes,
+# without fee, and without a written agreement is hereby granted, provided that
+# the above copyright notice, this paragraph and the following three paragraphs
+# appear in all copies. Permission to make commercial use of this software may
+# be obtained by contacting:
 #
-# Copyright 2022 CW3E, Contact Colin Grudzien cgrudzien@ucsd.edu
+#     Office of Innovation and Commercialization
+#     9500 Gilman Drive, Mail Code 0910
+#     University of California
+#     La Jolla, CA 92093-0910
+#     innovation@ucsd.edu
+#
+# This software program and documentation are copyrighted by The Regents of the
+# University of California. The software program and documentation are supplied
+# "as is", without any accompanying services from The Regents. The Regents does
+# not warrant that the operation of the program will be uninterrupted or
+# error-free. The end-user understands that the program was developed for
+# research purposes and is advised not to rely exclusively on the program for
+# any reason.
+#
+# IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+# DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+# LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION,
+# EVEN IF THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE. THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED
+# HEREUNDER IS ON AN “AS IS” BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO
+# OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+# MODIFICATIONS.
 # 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-#     Unless required by applicable law or agreed to in writing, software
-#     distributed under the License is distributed on an "AS IS" BASIS,
-#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#     See the License for the specific language governing permissions and
-#     limitations under the License.
 # 
 ##################################################################################
 # Imports
